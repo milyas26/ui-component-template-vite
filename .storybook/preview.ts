b4964ea9bb9ede7a344789973411f58c20450cd4
Muhammad Ilyas <muhammad.ilyas@jubelio.com>
2025-02-27 14:58:03 +0700

    fix: update preview storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,13 +3,13 @@ import "../lib/tailwind/theme.css";
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
+    // actions: { argTypesRegex: "^on[A-Z].*" },
+    // controls: {
+    //   matchers: {
+    //     color: /(background|color)$/i,
+    //     date: /Date$/,
+    //   },
+    // },
   },
 
   tags: ["autodocs"],
